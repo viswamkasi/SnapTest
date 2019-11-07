316b859af219e8edacc23d4e2bf9aa79808a15c4
Your Name <you@example.com>
2019-11-07 21:08:56 +0100

    Adding files to repo

---
+++ SnapEngage/src/test/java/testCases/Login_test.java
@@ -0,0 +1,18 @@
+package testCases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import commonFunctions.CommonFunctions;
+import pageObjects.Login_Page_Objects;
+
+public class Login_test extends CommonFunctions {
+
+	
+	@Test
+	public void Login_test() {
+		
+	}
+
+}
